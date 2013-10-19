e0721baaaa6fec654484b4ab5c7cd99a0e87a369
Emmanuel Nelson <elnels@gmail.com>
2013-10-19 16:31:30 -0500

    configuration

--- iShare.DataContext/Migrations/Configuration.cs
+++ iShare.DataContext/Migrations/Configuration.cs
@@ -6,8 +6,8 @@ namespace iShare.DataContext.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
-            AutomaticMigrationDataLossAllowed = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(DataContext context)
